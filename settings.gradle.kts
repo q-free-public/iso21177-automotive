0ffc6e8174b674eb03652961d82cb62f9cb9f633
Ola Martin Lykkja <ola.lykkja@q-free.com>
2023-12-11 09:32:52 +0100

    First runnable version from Android Studio template

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ISO 21177 poc"
+include(":mobile")
+include(":automotive")
